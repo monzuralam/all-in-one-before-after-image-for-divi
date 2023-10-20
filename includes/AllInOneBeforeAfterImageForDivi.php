7054a12df4daf3b0b3b827c2c193eb206a6b031f
monzuralam <monzur707@gmail.com>
2023-10-20 19:32:56 +0600

    register style & scripts

--- includes/AllInOneBeforeAfterImageForDivi.php
+++ includes/AllInOneBeforeAfterImageForDivi.php
@@ -35,11 +35,26 @@ class AIOBAI_AllInOneBeforeAfterImageForDivi extends DiviExtension {
 	 * @param string $name
 	 * @param array  $args
 	 */
-	public function __construct( $name = 'all-in-one-before-after-image-for-divi', $args = array() ) {
-		$this->plugin_dir     = plugin_dir_path( __FILE__ );
-		$this->plugin_dir_url = plugin_dir_url( $this->plugin_dir );
+	public function __construct($name = 'all-in-one-before-after-image-for-divi', $args = array()) {
+		$this->plugin_dir     = plugin_dir_path(__FILE__);
+		$this->plugin_dir_url = plugin_dir_url($this->plugin_dir);
 
-		parent::__construct( $name, $args );
+		parent::__construct($name, $args);
+
+		add_action('wp_enqueue_scripts', array($this, 'aiobai_assets'));
+	}
+
+	/**
+	 * Assets
+	 *
+	 * @return void
+	 */
+	public function aiobai_assets() {
+		wp_register_style('aiobai-twentytwenty', $this->plugin_dir_url . 'assets/css/twentytwenty.css', array(), $this->version, 'screen');
+
+		wp_register_script('aiobai-event-move', $this->plugin_dir_url . 'assets/js/jquery.event.move.js', array('jquery'), $this->version, true);
+		wp_register_script('aiobai-twenty', $this->plugin_dir_url . 'assets/js/jquery.twentytwenty.js', array('jquery'), $this->version, true);
+		wp_register_script('aiobai-main', $this->plugin_dir_url . 'assets/js/main.js', array('jquery'), $this->version, true);
 	}
 }
 
