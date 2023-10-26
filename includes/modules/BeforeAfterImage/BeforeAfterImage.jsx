41731806c8cebde4ed8d4e391d5ab83d800007ab
monzuralam <monzur707@gmail.com>
2023-10-26 07:57:03 +0600

    script & style fix on frontend editor

--- includes/modules/BeforeAfterImage/BeforeAfterImage.jsx
+++ includes/modules/BeforeAfterImage/BeforeAfterImage.jsx
@@ -4,9 +4,32 @@ import React, { Component } from "react";
 import { PlaceHolderImage } from "./PlaceHolderImage";
 
 // Internal Dependencies
+import "../../../assets/css/twentytwenty.css";
 import "./style.css";
 
 class BeforeAfterImage extends Component {
+  constructor(props){
+    super(props);
+    this.scriptRef = React.createRef();
+  }
+
+  componentDidMount(){
+    this.loadScript('jquery.event.move');
+    this.loadScript('jquery.twentytwenty');
+    this.loadScript('main');
+  }
+
+  loadScript = (file) => {
+    const script = document.createElement('script');
+    script.src = `/wp-content/plugins/all-in-one-before-after-image-for-divi/assets/js/${file}.js`;
+
+    script.onload = () => {
+      console.log('loading script');
+    }
+
+    document.head.appendChild(script);
+  }
+
   static slug = "aiobai_before_after_image";
 
   _render_before_after_image = () => {
@@ -15,7 +38,7 @@ class BeforeAfterImage extends Component {
       after_image = props.after_image ? props.after_image : PlaceHolderImage;
 
     return (
-      <div className="aiobai-image">
+      <div className="aiobai-image" id="aiobai-init">
         <img src={before_image} alt="aiobai" />
         <img src={after_image} alt="aiobai" />
       </div>
